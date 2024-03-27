32ef6b48a345a6e6b65ba15d8f791bd55fe9c333
JoeKaleli <kalelimburu9@gmail.com>
2024-03-27 13:07:15 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', TRUE),
+('Rowdy', 3, 'M', 'Black', 'Playful', TRUE),
+('Tabitha', 7, 'F', 'Gray', 'Calm', TRUE),
+('Sergeant Brown', 8, 'M', 'Brown', 'Grumpy', TRUE),
+('Melissa', 4, 'F', 'Golden', 'Energetic', TRUE),
+('Grinch', 6, 'M', 'Green', 'Sneaky', TRUE),
+('Wendy', 2, 'F', 'White', 'Shy', TRUE),
+('unnamed', NULL, NULL, NULL, NULL, TRUE);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,47 @@
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT
+     name,
+        age
+    FROM bears
+    WHERE sex = 'F';
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    
+     SELECT
+        name,
+        age,
+        color
+    FROM bears
+    WHERE age > 5;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+    
+    SELECT
+        name,
+        temperament
+    FROM bears
+    WHERE temperament IN ('Calm', 'Friendly');
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+    name,
+    age
+FROM
+    bears
+ORDER BY
+    age DESC
+LIMIT 1;
+
+
 """
-select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+select_youngest_bear_and_returns_name_and_sex = """
+    
+    SELECT
+        name,
+        sex
+    FROM bears
+    WHERE color = 'Brown';
 """
